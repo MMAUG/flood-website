ddb128c17d133361609de30b49b372d9b848cec7
Thuya Myo Nyunt <ty@kny.co>
2015-08-03 02:08:23 +0630

    When window is scroll down, it now query for next-page

--- public/js/app.js
+++ public/js/app.js
@@ -18,12 +18,10 @@ function closeModel() {
  */
 Vue.filter('truncate', function(value, length) {
 
-  if(value.length < length) {
+  if(value.length < length)
     return value;
-  }
 
   length = length - 3;
-
   return value.substring(0, length) + '...';
 });
 
@@ -33,35 +31,15 @@ var donationGroup = new Vue({
   el: '#donation-groups',
 
   data: {
-    groups: null,
+    groups: [],
     total: null,
-    loading: true,
+    loading: false,
+    nextPage: 1,
   },
 
   ready: function() {
   	// Request donation group from api server
-  	this.$http.get(donationGroupURL, function (data, status, request) {
-  		// Set groups data from api response data.
-      data = data.map(function(info){
-        info.description = info.description.replace($REG_URL, function(match, url){
-          return "<a href='"+ url +"'> "+url+" </a>";
-        });
-
-        info.title = kny.fontConvert(info.title, "unicode5");
-        info.description = kny.fontConvert(info.description, "unicode5");
-        // info.content = kny.fontConvert(info.content, "unicode5");
-
-        return info;
-      });
-
-  		this.$set('groups', data);
-      // Set total groups
-      this.$set('total', data.length);
-      // Set loading is false
-      this.$set('loading', false);
-  	}).error(function (data, status, request) {
-
-  	});
+  	this.update();
   },
 
   methods: {
@@ -87,6 +65,42 @@ var donationGroup = new Vue({
 
       $('#close-modal').on('click', function() {
         closeModel();
+      });
+    },
+    update: function(){
+      var nextPage = this.$get("nextPage");
+      if( this.$get("loading") || nextPage === "end" ) {
+        return true;
+      }
+
+      this.$set("loading", true);
+      this.$http.get(donationGroupURL + "?page=" + nextPage, function (data, status, request) {
+        // Set groups data from api response data.
+
+        if( data === "" || !data.length || data.length === 0){
+          this.$set("nextPage", "end");
+          this.$set('loading', false);
+          return true;
+        }
+
+        this.$set("nextPage", nextPage + 1);
+
+        data = data.map(function(info){
+          info.title = kny.fontConvert(info.title, "unicode5");
+          info.description = kny.fontConvert(info.description, "unicode5");
+          info.donation_location = kny.fontConvert(info.donation_location, "unicode5");
+          return info;
+        });
+
+        data = this.$get("groups").concat(data);
+
+        this.$set('groups', data);
+        // Set total groups
+        this.$set('total', data.length);
+        // Set loading is false
+        this.$set('loading', false);
+      }).error(function (data, status, request) {
+
       });
     }
   }
@@ -136,15 +150,16 @@ var newDonationGroup = new Vue({
         // Hide posting label...
         this.newIsPosting = false;
 
-        // Redirect to home page.
-        document.location.href="/";
+        this.description = "";
+        this.phone_numbers = "";
+        this.donation_location = "";
+        this.facebook_url = "";
 
       }, {emulateJSON: true}).error(function (data, status, request) {
         alert('Error: Please try again to post your donation group...');
       });
     }
   }
-
 });
 
 // News feed
@@ -153,34 +168,17 @@ var newsfeeds = new Vue({
   el: '#new-feed',
 
   data: {
-    news: null,
-    loading: true,
+    news: [],
+    loading: false,
     invalidTitle: false,
     invalidDescription: false,
-    newIsPosting: false
+    newIsPosting: false,
+    nextPage: 1
   },
 
   ready: function() {
     // Request donation group from api server
-    this.$http.get(newsURL, function (data, status, request) {
-
-      data = data.map(function(info){
-        info.description = info.description.replace($REG_URL, function(match, url){
-          return "<a href='"+ url +"'> "+url+" </a>";
-        });
-
-        info.title = kny.fontConvert(info.title, "unicode5");
-        info.description = kny.fontConvert(info.description, "unicode5");
-
-        return info;
-      });
-      // Set news data from api response data.
-      this.$set('news', data);
-      // Set loading is false
-      this.$set('loading', false);
-    }).error(function (data, status, request) {
-
-    });
+    this.update();
   },
 
   methods: {
@@ -217,7 +215,8 @@ var newsfeeds = new Vue({
         // Add server response data to current news data.
         this.news.unshift(data);
 
-        document.location.reload();
+        this.title = "";
+        this.description = "";
 
       }, {emulateJSON: true}).error(function (data, status, request) {
         alert('Error: Please try again to post your new...');
@@ -233,6 +232,62 @@ var newsfeeds = new Vue({
       }).error(function (data, status, request) {
         alert('Error: Please try again to report...');
       });
+    },
+    update : function(e){
+      var nextPage = this.$get("nextPage");
+      if( this.$get("loading") || nextPage === "end" ) {
+        return true;
+      }
+
+      this.$set("loading", true);
+      this.$http.get(newsURL + "?page=" + this.nextPage, function (data, status, request) {
+
+        if( data === "" || !data.length || data.length === 0){
+          this.$set("nextPage", "end");
+          this.$set('loading', false);
+          return true;
+        }
+
+        this.$set("nextPage", nextPage + 1);
+
+        data = data.map(function(info){
+          info.title = kny.fontConvert(info.title, "unicode5");
+          info.description = kny.fontConvert(info.description, "unicode5");
+          return info;
+        });
+
+        data = this.$get("news").concat(data);
+        // Set news data from api response data.
+        this.$set('news', data);
+        // Set loading is false
+        this.$set('loading', false);
+      }).error(function (data, status, request) {
+
+      });
+    }
+  }
+});
+
+
+/**
+ * Scorll down to the end will query next page
+ */
+var pageinfo = document.body.getAttribute("data-page");
+var mainContainer = $("#main-container");
+var mainContent = $("#main-content");
+
+mainContainer.scroll(function() {
+  var sheight = mainContainer.scrollTop() + mainContainer.height();
+  var height = mainContent.height() - 50;
+
+  if( sheight > height) {
+    switch(pageinfo){
+      case "donate":
+        donationGroup.update();
+        break;
+      case "news":
+        newsfeeds.update();
+        break;
     }
   }
 });
