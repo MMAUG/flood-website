049d979eb3f0e837f25d718ad1054571f8c5ab28
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-08-02 18:38:53 +0630

    concat vendor js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var del = require('del');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+  return gulp.src([
+  		'./public/js/jquery-1.11.3.min.js', 
+  		'./public/js/material.min.js', 
+  		'./public/js/vue.min.js',
+  		'./public/js/vue-resource.min.js',
+  	])
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('default', ['scripts']);
