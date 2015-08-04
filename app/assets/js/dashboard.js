0e51970b5fe420626ee707136181cf97c98882b7
winhtaikaung <winhtaikaung28@hotmail.com>
2015-08-04 16:18:44 +0630

    sample map only needed to bind data to map

--- app/assets/js/dashboard.js
+++ app/assets/js/dashboard.js
@@ -45,9 +45,51 @@ $(function(){
 	            enableMouseTracking: false
 	        }];
 
+	        map_current_status_2={};
+	  	map_current_status_2.title="Map 2";
+	  	map_current_status_2.divarea="#sample_map_2";
+	  	map_current_status_2.mapData=geojson_collection_LV3;
+	  	map_current_status_2.mapseries=[{
+	            // Use the gb-all map with no data as a basemap
+	            mapData: map_current_status_2.mapData,
+	            name: 'Basemap',
+	            borderColor: '#A0A0A0',
+	            nullColor: 'rgba(200, 200, 200, 0.3)',
+	            showInLegend: false
+	        }, {
+	            name: 'Separators',
+	            type: 'mapline',
+	            data: Highcharts.geojson(map_current_status_2.mapData, 'mapline'),
+	            color: '#707070',
+	            showInLegend: false,
+	            enableMouseTracking: false
+	        }];
+
+
+map_current_status_3={};
+	  	map_current_status_3.title="Provincial Possible flood rate";
+	  	map_current_status_3.divarea="#sample_map_3";
+	  	map_current_status_3.mapData=geojson_collection_LV1;
+	  	map_current_status_3.mapseries=[{
+	            // Use the gb-all map with no data as a basemap
+	            mapData: map_current_status_3.mapData,
+	            name: 'Basemap',
+	            borderColor: '#A0A0A0',
+	            nullColor: 'rgba(200, 200, 200, 0.3)',
+	            showInLegend: false
+	        }, {
+	            name: 'Separators',
+	            type: 'mapline',
+	            data: Highcharts.geojson(map_current_status_3.mapData, 'mapline'),
+	            color: '#707070',
+	            showInLegend: false,
+	            enableMouseTracking: false
+	        }];
 	    //do mapdata binding here
 
 
 	  	map_maphelper.map_lat_lon_drawer(map_current_status);
+	  	map_maphelper.map_lat_lon_drawer(map_current_status_2);
+	  	map_maphelper.map_lat_lon_drawer(map_current_status_3);
 	  }
 });
