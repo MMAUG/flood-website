c001991a2f49171c98cabf9b979c340b5d9c9728
Thuya Myo Nyunt <ty@kny.co>
2015-08-06 02:01:14 +0630

    NODE stuff change, reinit

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,15 @@ var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
-var ejs = require("gulp-ejs");
 var gutil = require("gulp-util");
 
 gulp.task('watch', function(){
   gulp.watch("gulpfile.js", ["scripts"]);
   gulp.watch("./app/assets/js/*", ['scripts']);
-  gulp.watch("./app/templates/partials/*", ['templates']);
-  gulp.watch("./app/templates/*", ['templates']);
 });
 
 gulp.task('scripts', function() {
-  return gulp.src([
+  gulp.src([
     './app/assets/js/jquery-1.11.3.min.js',
     './app/assets/js/vendor_highmaps.js',
     './app/assets/js/moment.js',
@@ -26,24 +23,24 @@ gulp.task('scripts', function() {
     './app/assets/js/knayi-myscript.js',
     './app/assets/js/search.js',
     './app/assets/js/campaign-data.js',
-    './app/assets/js/marker_clusterer.js',
     './app/assets/js/app.js',
-    './app/assets/js/floods_map.js',
     './app/assets/js/dashboard.js'
   ])
   // .pipe(uglify())
   .pipe(concat('app.min.js'))
   .pipe(gulp.dest('./public/js/'));
+
+  gulp.src([
+    './app/assets/js/marker_with_label_packed.js',
+    './app/assets/js/marker_clusterer.js',
+    './app/assets/js/floods_map.js',
+  ])
+  // .pipe(uglify())
+  .pipe(concat('map.min.js'))
+  .pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('templates', function(){
-  gulp.src(["./app/templates/*.ejs", "!./app/templates/partials/*.ejs"])
-  .pipe(ejs({
-    msg: "Hello Gulp!"
-  }, {ext: '.html'}).on('error', gutil.log))
-  .pipe(gulp.dest("./app/views"));
-});
 
-gulp.task('default', ['scripts', 'templates', 'watch']);
+gulp.task('default', ['scripts', 'watch']);
 
-gulp.task('production', ['scripts', 'templates']);
+gulp.task('production', ['scripts']);
