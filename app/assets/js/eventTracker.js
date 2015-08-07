2456d493ac2f4a21174a35a89de49765fb1ae77b
Thuya Myo Nyunt <ty@kny.co>
2015-08-07 23:28:37 +0630

    Add google analytics events tracker js

---
+++ app/assets/js/eventTracker.js
@@ -0,0 +1,11 @@
+(function(){
+  $(".mdl-layout__drawer-button").click(function(event){
+    ga('send', 'event', 'button', 'click', 'menu-button-header');
+  });
+
+  $("[data-ga]").click(function(event){
+    var args = ['send', 'event'];
+    args.concat( $(this).attr('data-ga').split("|") );
+    ga.apply(null, args);
+  });
+}());
